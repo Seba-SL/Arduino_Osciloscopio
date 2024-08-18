c8029eae5afc57a23a6b5660df98b789631e4928
Seba-SL <slazo@fi.uba.ar>
2024-08-18 10:02:09 -0300

    Subida del codigo

---
+++ Código/osciloscroto.ino
@@ -0,0 +1,28 @@
+// Definir los pines analógicos a los que están conectadas las señales
+
+const float pinSignal1 = A0; // Primer señal en el pin A0
+const float pinSignal2 = A1; // Segunda señal en el pin A1
+
+void setup() 
+{
+  // Iniciar la comunicación serial
+  Serial.begin(115200);
+  pinMode(pinSignal1,INPUT),
+  pinMode(pinSignal2,INPUT);
+}
+
+void loop() {
+  // Leer las señales de los pines analógicos
+  float valorSignal1 = analogRead(pinSignal1);
+  float valorSignal2 = analogRead(pinSignal2);
+  
+  // Enviar los valores leídos al monitor serial
+  Serial.print("Signal_A:");
+  Serial.println(valorSignal1);
+  
+  Serial.print("Signal_B:");
+  Serial.println(valorSignal2);
+ 
+
+  delay(500); // 500 milisegundos (0.5 segundos)
+}
